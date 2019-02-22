f5b7c04942521216be474a3253c31f06187d9b80
Tom Johnell <tjohnell@handy.com>
2019-02-21 19:59:14 -0500

    Added cryptonite and example

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,13 @@
 import Web.Scotty
 
 import Data.Monoid (mconcat)
+import Crypto.PubKey.RSA
+import Control.Monad.IO.Class(liftIO)
+import Data.Text.Lazy(pack)
 
 main = scotty 3000 $
     get "/:word" $ do
-        beam <- param "word"
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+        (public,private) <- liftIO $ generate 128 3
+        liftIO $ print public
+        liftIO $ print private
+        html $ pack $ show public
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import Crypto.PubKey.RSA
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    (public,private) <- generate 1024 2
+    print public
+    print private
